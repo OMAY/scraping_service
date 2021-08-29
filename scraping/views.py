b997f8b7ca26c6d7edbec56af123c5e2c27133c2
Yipman <mayvaldoff@gmail.com>
2021-08-29 04:11:12 +0300

    added pagination to list_view

--- scraping/views.py
+++ scraping/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
 from .forms import FindForm
@@ -9,11 +10,12 @@ def home_view(request):
 
     return render(request, 'scraping/home.html', {'form': form})
 
+
 def list_view(request):
     form = FindForm()
     city = request.GET.get('city')
     language = request.GET.get('language')
-    qs = []
+    context = {'city': city, 'language': language, 'form': form}
     if city or language:
         _filter = {}
         if city:
@@ -21,4 +23,11 @@ def list_view(request):
         if language:
             _filter['language__slug'] = language
         qs = Vacancy.objects.filter(**_filter)
-    return render(request, 'scraping/list.html', {'object_list': qs, 'form': form})
+
+        paginator = Paginator(qs, 10)  # Show 25 contacts per page.
+
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        context['object_list'] = page_obj
+
+    return render(request, 'scraping/list.html', context)
