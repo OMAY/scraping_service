8a601b8e952b5c756824f701585fbb4b5b2af1c9
Yipman <mayvaldoff@gmail.com>
2021-08-31 08:14:48 +0300

    attempt 2

--- scraping_service/settings/production.py
+++ scraping_service/settings/production.py
@@ -36,7 +36,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['serv-scrap-1.herokuapp.com']
+ALLOWED_HOSTS = ['scrap-serv-2.herokuapp.com']
 
 
 # Application definition
